c07bfa8e6206cd7bfc898348ee886ba681eb3fc2
Dmitry Burlakov <dantaeusb@live.com>
2017-01-06 16:19:00 +0300

    Improve comments overlay

---
+++ dist/js/overlay.ts
@@ -0,0 +1,8 @@
+/**
+ * Created by dantaeusb on 06/01/2017.
+ */
+
+class Overlay {
+    protected id: Number;
+
+}
---
+++ public/js/overlay.js
@@ -0,0 +1,9 @@
+/**
+ * Created by dantaeusb on 06/01/2017.
+ */
+var Overlay = (function () {
+    function Overlay() {
+    }
+    return Overlay;
+}());
+//# sourceMappingURL=overlay.js.map
